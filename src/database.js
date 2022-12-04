da597ba15cefe805faaa5f9a6bba053658a2e1f0
Sergi <cuentasps481@gmail.com>
2022-12-03 22:59:13 -0600

    make it better

--- src/database.js
+++ src/database.js
@@ -4,6 +4,7 @@ const { mongodb } = require('./keys');
 mongoose.set('useFindAndModify', false);
 mongoose.connect(mongodb.URI, {
   useNewUrlParser: true
+  
 })
   .then(db => console.log('DB is connected'))
   .catch(err => console.log(err));
